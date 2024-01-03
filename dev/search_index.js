3aebf9bc87675fa2c7525ab71b7f0713ea726456
Documenter.jl <documenter@juliadocs.github.io>
2024-01-03 10:36:09 +0000

    build based on 412eb72

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OrUnions","category":"page"},{"location":"#OrUnions","page":"Home","title":"OrUnions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OrUnions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OrUnions]","category":"page"}]
+}
